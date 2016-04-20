49409284d2db8794ed627efe27cbcc7e0a831f12
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-04-20 15:07:24 +0200

    Ask for location

--- main.py
+++ main.py
@@ -6,19 +6,25 @@ class M(FalconMessenger, Confirmation):
     def __init__(self, redis_client, *args, **kwargs):
         self.redis = redis_client
         super(M, self).__init__(*args, **kwargs)
-        
+
 
     def transform_message(self, recipient_id, text, attachments):
         logging.getLogger('aa').error('transform_message(%s, %s, %s)'.format(recipient_id, text, attachments))
         if not self.check_customer_confirmed(recipient_id):
             return self.confirm_customer(recipient_id, text)
-        if text:
-            logging.getLogger('a').error('text: {}'.format(text))
-            return 'got text'
-        elif attachments:
-            logging.getLogger('a').error('attachments: {}'.format(attachments))
-            return 'got attachments'
-        return 'got nothing'
+        id_ = 'localization:{}'.format(recipient_id)
+        if not redis.exists(id_):
+            redis.set(id_, 'location_asked')
+            return 'À quelle adresse souhaitez-vous un taxi ?'
+        else:
+            if text:
+                logging.getLogger('a').error('text: {}'.format(text))
+                return 'Requesting BAN to locate address...'
+            elif attachments:
+                # Position
+                logging.getLogger('a').error('attachments: {}'.format(attachments))
+                return 'got attachments'
+            return 'got nothing'
 
 
 app = falcon.API()
